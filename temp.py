12413c9f0b481c7bd8585c248a2ab6612cd7bc52
Freelancer-98 <neel12@somaiya.edu>
2019-02-10 20:18:26 +0530

    gswa day1 wshop added

--- temp.py
+++ temp.py
@@ -1,22 +1,46 @@
-q = int(input())
-numL = 0
-numR = 0
-leftId = {}
-rightId = {}
-for i in range(q):
-  pos,id = input().split()
-  if pos == "L":
-    numL += 1
-    leftId[id] = numL
-  elif pos == "R":
-    numR += 1
-    rightId[id] = numR
+n = int(input())
+ogStringFront = ""
+ogStringBack = ""
+totalSP = (2*n)-2
+SPs = []
+for i in range(totalSP):
+  SPs.append(input())
+SPs = list(enumerate(SPs))
+SPs.sort(key=lambda s: len(s[1]))
+# print(SPs)
+ans = ["" for i in range(totalSP)]
+flag = False
+for i in range(totalSP):
+  if i==0:
+    ogStringFront = SPs[i][1]
+    ans[SPs[i][0]] = "P"
+  elif i==1:
+    ogStringBack = SPs[i][1]
+    ans[SPs[i][0]] = "S"
   else:
-    if id in leftId.keys():
-      fromLeft = numL - leftId[id]
-      fromRight = numR + leftId[id] - 1
-      print(min(fromLeft,fromRight))
+    if ogStringFront == SPs[i][1][0:i//2]:
+      ogStringFront = SPs[i][1]
+      ans[SPs[i][0]] = "P"
+    elif ogStringBack == SPs[i][1][::-1][0:i//2][::-1]:
+      ogStringBack = SPs[i][1]
+      ans[SPs[i][0]] = "S"
     else:
-      fromLeft = numL + rightId[id] - 1
-      fromRight = numR - rightId[id]
-      print(min(fromLeft,fromRight))
+      flag = not flag
+      temp = ogStringFront
+      ogStringFront = ogStringBack
+      ogStringBack = temp
+      if ogStringFront == SPs[i][1][0:i//2]:
+        ogStringFront = SPs[i][1]
+        ans[SPs[i][0]] = "P"
+      elif ogStringBack == SPs[i][1][::-1][0:i//2][::-1]:
+        ogStringBack = SPs[i][1]
+        ans[SPs[i][0]] = "S"
+# print("".join(ans))
+# print(flag)
+if flag:
+  for i in range(len(ans)):
+    if ans[i] == "S":
+      ans[i] = "P"
+    else:
+      ans[i] = "S"
+print("".join(ans))
