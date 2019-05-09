dd5798e6600c00efcbbf211a23967b1a160ae6ae
nilshah98 <neel12@somaiya.edu>
2019-05-10 01:17:00 +0530

    Day 9 upda

--- codeforces/1092C.py
+++ codeforces/1092C.py
@@ -10,6 +10,8 @@ words.sort()
 flag = True
 curr = words[0][1]
 
+# print(words)
+
 for i in range(2,len(words),2):
     if words[i][1] == curr[:n - i//2 - 1] or words[i+1][1] == curr[:n - i//2 - 1]:
         continue
@@ -17,16 +19,17 @@ for i in range(2,len(words),2):
         flag = False
         break
 
-if flag:
+if flag and words[0][1][1:] == words[1][1][:-1] :
     word = words[0][1] + words[1][1][-1]
     words[0].append("P")
     words[1].append("S")
-
 else:
     word = words[1][1] + words[0][1][-1]
     words[0].append("S")
     words[1].append("P")
 
+
+# print(word)
 
 for i in range(2,len(words),2):
     if words[i][1] == word[:n - i//2 - 1]:
